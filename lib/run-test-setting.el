7d2f665b23e0cc379afb8b1661c01079fe94a1ad
kou <kou@41500763-7a30-dd11-b1b4-0010dcdd6dc2>
2010-05-28 00:18:20 +0000

    * specify license explicitly..

--- lib/run-test-setting.el
+++ lib/run-test-setting.el
@@ -1,4 +1,17 @@
-;;; run-test-setting.el
+;;; Copyright (C) 2008-2010  Kouhei Sutou <kou@cozmixng.org>
+;;;
+;;; This program is free software: you can redistribute it and/or modify
+;;; it under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation, either version 3 of the License, or
+;;; (at your option) any later version.
+;;;
+;;; This program is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ;; autoload
 (autoload 'run-test "run-test" nil t)
@@ -9,5 +22,3 @@
 (define-key global-map "\C-c\C-t" 'run-test)
 (define-key global-map "\C-cT" 'run-test-in-new-frame)
 (define-key global-map "\C-[\M-\C-t" 'run-test-in-mini-buffer)
-
-;;; end of file
